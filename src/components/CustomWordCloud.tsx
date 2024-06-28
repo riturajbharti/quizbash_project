0aa373f2428fdb5aea5de5644c8fc2542f9a7110
rituraj <riturajbharti99@gmail.com>
2024-06-28 16:03:27 +0530

    v1 Development Completed

--- src/components/CustomWordCloud.tsx
+++ src/components/CustomWordCloud.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useTheme } from 'next-themes'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 import D3WordCloud from 'react-d3-cloud'
 
